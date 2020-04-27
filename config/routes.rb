1767dcad9a20f3bbceba926fe48a43064bc9835b
Akhil <akhil.latta@trantorinc.com>
2020-04-28 02:43:45 +0530

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'links#new'
+
+  get '/:slug', to: 'links#show'
+
+  resources :links, only: [:create]
+
 end
