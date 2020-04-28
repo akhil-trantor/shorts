3dce8a6c738fa8abc5c7fcbb1e30c6cd31e11d26
Akhil <akhil.latta@trantorinc.com>
2020-04-28 14:43:36 +0530

    added analytics methods concern file

---
+++ app/models/concerns/analytics_methods.rb
@@ -0,0 +1,10 @@
+module AnalyticsMethods
+  extend ActiveSupport::Concern
+
+  def set_analytics_data(request)
+    self.ip_address = request.ip
+    self.country    = request.location.country
+    self.agent      = request.user_agent
+  end
+
+end
